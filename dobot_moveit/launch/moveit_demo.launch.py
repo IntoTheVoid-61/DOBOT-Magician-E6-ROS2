958ff32c5cc67ef6a167820e360db0f7bfd4f221
Ziga Breznikar <ziga.breznikar@student.um.si>
2026-03-21 13:03:15 +0100

    Added move_to_pose_action, system test for framework

--- dobot_moveit/launch/moveit_demo.launch.py
+++ dobot_moveit/launch/moveit_demo.launch.py
@@ -1,8 +1,14 @@
+"""
+@brief Official dobot launch file for moveit demo, also including:
+    - move_to_pose_action server
+
+"""
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 import os
 from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
 def generate_launch_description():
     name = os.getenv("DOBOT_TYPE")
     package_name = f'{name}_moveit'
@@ -15,7 +21,16 @@ def generate_launch_description():
     included_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(moveit_model_path)
     )
+
+    move_to_pose_action_server_node = Node(
+        package='dobot_system_tests',
+        executable='move_to_pose_action_server',
+        output='screen'
+    )
+
+
     ld.add_action(included_launch)
+    ld.add_action(move_to_pose_action_server_node)
 
     # 添加其他操作...
     return ld
